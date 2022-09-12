00f007919bab9b11ad44b06ec7113f0a29d88348
User1 <br4vespirit@gmail.com>
2022-09-12 21:54:36 +0200

    initialized project

---
+++ src/main/java/com/dima/springsecuritystudying/SpringSecurityStudyingApplication.java
@@ -0,0 +1,13 @@
+package com.dima.springsecuritystudying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStudyingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityStudyingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dima/springsecuritystudying/SpringSecurityStudyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dima.springsecuritystudying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStudyingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
