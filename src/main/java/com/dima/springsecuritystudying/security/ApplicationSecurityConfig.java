e3285a8c2ea3bee51759d3fec7ca14205250c400
User1 <br4vespirit@gmail.com>
2022-09-14 20:47:49 +0200

    InMemoryUserDetailsManager logic implemented

--- src/main/java/com/dima/springsecuritystudying/security/ApplicationSecurityConfig.java
+++ src/main/java/com/dima/springsecuritystudying/security/ApplicationSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +23,13 @@ public class ApplicationSecurityConfig {
                 .and()
                 .httpBasic()
                 .and().build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("user1").password("pass1").roles("STUDENT").build();
+
+        return new InMemoryUserDetailsManager(user1);
     }
 }
